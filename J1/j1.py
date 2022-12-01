891b73a601e37d049b9cfae108da0a68f5abcdc2
UltiXstorm <ultixstorm@gmail.com>
2022-12-01 19:54:50 +0100

    j1

---
+++ J1/j1.py
@@ -0,0 +1,19 @@
+print('Start J1')
+n = 0
+elfs_capacity = []
+with open('./input.txt', 'r') as input_file:
+    current_capacity = 0
+    for line in input_file:
+        if(line == '\n'):
+            elfs_capacity.append(current_capacity)
+            current_capacity = 0
+        else:
+            current_capacity += int(line)
+        n += 1
+        print(n)
+
+
+max_capacity = max(elfs_capacity)
+elf_number = elfs_capacity.index(max_capacity)
+
+print("Max calories is {} carried by elf number {}".format(max_capacity, elf_number+1))
